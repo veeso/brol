60c36eef8a21b59bbe7f2b1e89bb88a04e3e2600
veeso <christian.visintin1997@gmail.com>
2021-06-01 17:27:39 +0200

    bla

--- rust/open/src/main.rs
+++ rust/open/src/main.rs
@@ -15,25 +15,39 @@
  *
  *   0. You just DO WHAT THE FUCK YOU WANT TO.
 */
-
 extern crate open;
 
 use std::env;
-use std::io::Result as IoResult;
 use std::path::PathBuf;
-use std::process::ExitStatus;
-use std::thread::JoinHandle;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        panic!("Usage: {} <file>", args[0]);
+        panic!("Usage: {} <file> [open-with]", args[0]);
     }
     let file: PathBuf = PathBuf::from(args[1].as_str());
+    let open_with: Option<String> = match args.get(2) {
+        Some(s) => Some(s.to_string()),
+        None => None,
+    };
+    /*
+    println!("Open async...");
     let join: JoinHandle<IoResult<ExitStatus>> = open::that_in_background(file.as_path());
     println!("Opened {}", file.display());
     match join.join() {
         Ok(rc) => println!("Process exited with {:?}", rc),
         Err(err) => eprintln!("Failed to open file: {:?}", err),
+    }
+    */
+    match open::that(file.as_path()) {
+        Ok(rc) => println!("Process exited with {:?}", rc),
+        Err(err) => eprintln!("Failed to open file: {:?}", err),
+    }
+    if let Some(open_with) = open_with {
+        println!("Open with {}", open_with);
+        match open::with(file.as_path(), open_with) {
+            Ok(rc) => println!("Process exited with {:?}", rc),
+            Err(err) => eprintln!("Failed to open file: {:?}", err),
+        }
     }
 }
