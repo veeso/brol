e9a73fcd0a7b8b3abd60089eca1c0502ea03e240
veeso <christian.visintin1997@gmail.com>
2021-06-23 17:51:12 +0200

    We're no strangers to love

--- python/fibonacci/fibonaccy.py
+++ python/fibonacci/fibonaccy.py
@@ -24,29 +24,17 @@ from typing import List
 def fibonacci(n: int) -> int:
     if n == 0:
         return 0
-    elif n < 3:
+    if n == 1:
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
-
-def curious_fibonacci(n: int) -> int:
-    if n == 0:
-        return 0
-    elif n < 3:
-        return 1
-    return ((curious_fibonacci(n - 2) * 2) + curious_fibonacci(n - 3))
 
 def main(argc: int, argv: List[str]) -> int:
     # Get options
     if argc < 1:
         print("Usage: fibonaccy.py <n>")
         return 255
-    recursions = int(argv[0])
-    if recursions < 3:
-        print("n must be 3 at least")
-        return 1
-    list(map(lambda x : print("%d" % fibonacci(x)), list(range(recursions))))
-    print("-- Curious fibonacci --")
-    list(map(lambda x : print("%d" % curious_fibonacci(x)), list(range(recursions))))
+    x = int(argv[0])
+    print("%d" % fibonacci(x))
     # Return success
     return 0
 
