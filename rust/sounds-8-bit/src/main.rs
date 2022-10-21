946eb54b4fee1478ee4bac13e1ee2e18763eaf1f
veeso <christian.visintin1997@gmail.com>
2022-10-21 16:11:40 +0200

    ajax-loader hotness, oh yeah

--- rust/sounds-8-bit/src/main.rs
+++ rust/sounds-8-bit/src/main.rs
@@ -103,19 +103,19 @@ impl Sound {
             Sound::ArmorEquipped => Self::armor_equipped(),
             Sound::DrinkPotion => Self::drink_potion(),
             Sound::EnemyApproaching => Self::enemy_approaching(),
-            Sound::EnemyAttack => todo!(),
-            Sound::EnemyScream => todo!(),
+            Sound::EnemyAttack => Self::enemy_attack(),
+            Sound::EnemyScream => Self::enemy_scream(),
             Sound::Error => Self::error(),
             Sound::Input => Self::input(),
             Sound::ItemCollected => Self::item_collected(),
-            Sound::LeaveMaze => todo!(),
-            Sound::PlayerAttack => todo!(),
-            Sound::PlayerDead => todo!(),
+            Sound::LeaveMaze => Self::leave_maze(),
+            Sound::PlayerAttack => Self::player_attack(),
+            Sound::PlayerDead => Self::player_dead(),
             Sound::Rush => Self::rush(),
             Sound::Sonar => Self::sonar(),
             Sound::Sleep => Self::sleep(),
             Sound::Steps => Self::steps(),
-            Sound::WakeUp => todo!(),
+            Sound::WakeUp => Self::wake_up(),
         }
     }
 
@@ -162,6 +162,20 @@ impl Sound {
             .tone(Note::Do.freq(2), 1000, 2.0)
     }
 
+    fn enemy_attack() -> Track {
+        Track::default()
+            .tone(50.0, 300, 15.0)
+            .tone(70.0, 100, 15.0)
+            .tone(100.0, 250, 15.0)
+    }
+
+    fn enemy_scream() -> Track {
+        Track::default()
+            .tone(100.0, 120, 20.0)
+            .tone(120.0, 120, 20.0)
+            .tone(140.0, 500, 20.0)
+    }
+
     fn error() -> Track {
         Track::default()
             .tone(150.0, 25, 1.0)
@@ -179,6 +193,33 @@ impl Sound {
             .tone(Note::As.freq(5), 200, 0.2)
             .tone(Note::Si.freq(5), 200, 0.2)
             .tone(Note::Do.freq(6), 500, 0.2)
+    }
+
+    fn leave_maze() -> Track {
+        Track::default()
+            .tone(Note::Mi.freq(4), 150, 1.0)
+            .tone(Note::Mi.freq(4), 150, 1.0)
+            .tone(Note::Mi.freq(4), 150, 1.0)
+            .tone(Note::La.freq(4), 750, 1.0)
+    }
+
+    fn player_attack() -> Track {
+        Track::default()
+            .tone(120.0, 130, 25.0)
+            .tone(110.0, 30, 25.0)
+    }
+
+    fn player_dead() -> Track {
+        Track::default()
+            .tone(Note::Ds.freq(5), 200, 0.3)
+            .tone(Note::D.freq(5), 200, 0.3)
+            .tone(Note::Ds.freq(5), 200, 0.3)
+            .tone(Note::D.freq(5), 200, 0.3)
+            .tone(Note::Ds.freq(5), 400, 0.3)
+            .tone(Note::D.freq(5), 400, 0.3)
+            .tone(Note::Ds.freq(5), 700, 0.3)
+            .tone(Note::D.freq(5), 700, 0.3)
+            .tone(Note::C.freq(4), 700, 1.0)
     }
 
     fn rush() -> Track {
@@ -230,6 +271,18 @@ impl Sound {
             .tone(10.0, 200, 4.0)
             .tone(0.0, 300, 0.2)
     }
+
+    fn wake_up() -> Track {
+        Track::default()
+            .tone(Note::G.freq(5), 700, 0.3)
+            .tone(Note::B.freq(5), 700, 0.3)
+            .tone(Note::A.freq(5), 700, 0.3)
+            .tone(Note::D.freq(5), 1000, 0.3)
+            .tone(Note::D.freq(5), 700, 0.3)
+            .tone(Note::A.freq(5), 700, 0.3)
+            .tone(Note::B.freq(5), 700, 0.3)
+            .tone(Note::G.freq(5), 1000, 0.3)
+    }
 }
 
 impl FromStr for Sound {
@@ -257,19 +310,131 @@ impl FromStr for Sound {
     }
 }
 
+/// Donmaze theme type
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum Theme {
+    Fight,
+    GameOver,
+    Maze,
+    Menu,
+    Victory,
+    None,
+}
+
+impl Theme {
+    pub fn track(self) -> Track {
+        match self {
+            Theme::Fight => Self::fight(),
+            Theme::GameOver => Self::game_over(),
+            Theme::Maze => Self::maze(),
+            Theme::Menu => Self::menu(),
+            Theme::Victory => Self::victory(),
+            Theme::None => Track::default(),
+        }
+    }
+
+    fn fight() -> Track {
+        Track::default()
+            .tone(Note::Cs.freq(2), 300, 4.0)
+            .tone(Note::Cs.freq(2), 300, 4.0)
+            .tone(Note::D.freq(2), 300, 4.0)
+            .tone(Note::Cs.freq(2), 300, 4.0)
+            .tone(Note::Cs.freq(2), 300, 4.0)
+            .tone(Note::D.freq(2), 300, 4.0)
+            .tone(Note::Cs.freq(2), 300, 4.0)
+            .tone(Note::Cs.freq(2), 300, 4.0)
+            .tone(Note::Ds.freq(2), 300, 4.0)
+            .tone(Note::E.freq(2), 600, 4.0)
+    }
+
+    fn game_over() -> Track {
+        Track::default()
+            .tone(Note::E.freq(4), 500, 0.8)
+            .tone(Note::D.freq(4), 500, 0.8)
+            .tone(Note::Cs.freq(4), 2000, 0.8)
+            .tone(Note::As.freq(3), 500, 2.0)
+            .tone(Note::Gs.freq(3), 500, 2.0)
+            .tone(Note::E.freq(3), 2000, 3.0)
+            .tone(Note::F.freq(3), 500, 2.0)
+            .tone(Note::Gs.freq(3), 1200, 2.0)
+            .tone(Note::C.freq(4), 1000, 0.8)
+    }
+
+    fn maze() -> Track {
+        Track::default()
+            .tone(51.9, 2600, 4.0)
+            .tone(116.6, 2600, 3.2)
+            .tone(123.2, 2600, 3.0)
+            .tone(116.6, 2600, 3.0)
+    }
+
+    fn menu() -> Track {
+        Track::default()
+            .tone(50.0, 300, 1.5)
+            .tone(70.0, 300, 2.5)
+            .tone(60.0, 300, 3.5)
+            .tone(30.0, 300, 4.5)
+            .tone(40.0, 300, 5.5)
+            .tone(30.0, 300, 4.5)
+            .tone(60.0, 300, 3.5)
+            .tone(50.0, 300, 1.5)
+            .tone(70.0, 300, 2.5)
+    }
+
+    fn victory() -> Track {
+        Track::default()
+            .tone(Note::E.freq(4), 500, 0.4)
+            .tone(Note::E.freq(4), 500, 0.4)
+            .tone(Note::F.freq(4), 500, 0.4)
+            .tone(Note::G.freq(4), 500, 0.4)
+            .tone(Note::Gs.freq(4), 500, 0.4)
+            .tone(Note::F.freq(4), 500, 0.4)
+            .tone(Note::E.freq(4), 500, 0.4)
+            .tone(Note::E.freq(4), 500, 0.4)
+            .tone(Note::F.freq(4), 500, 0.4)
+            .tone(Note::Gs.freq(4), 500, 0.4)
+            .tone(Note::Gs.freq(4), 500, 0.4)
+            .tone(Note::G.freq(4), 500, 0.4)
+            .tone(Note::F.freq(4), 500, 0.4)
+    }
+}
+
+impl FromStr for Theme {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "fight" => Ok(Self::Fight),
+            "gameover" => Ok(Self::GameOver),
+            "maze" => Ok(Self::Maze),
+            "menu" => Ok(Self::Menu),
+            "victory" => Ok(Self::Victory),
+            _ => Err("unknown theme"),
+        }
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let sound = match std::env::args().collect::<Vec<String>>().get(1) {
         Some(s) => s.to_string(),
         None => anyhow::bail!("usage: <sound>"),
     };
-    let sound = Sound::from_str(&sound).map_err(|e| anyhow::anyhow!(e))?;
     let (_stream, stream_handle) = OutputStream::try_default()?;
     let sink = Sink::try_new(&stream_handle)?;
-    for tone in sound.track().tones {
-        sink.append(tone);
+    if let Ok(sound) = Sound::from_str(&sound) {
+        for tone in sound.track().tones {
+            sink.append(tone);
+        }
+        sink.sleep_until_end();
+        sleep(Duration::from_millis(100));
     }
-    sink.sleep_until_end();
-    sleep(Duration::from_millis(100));
+    if let Ok(theme) = Theme::from_str(&sound) {
+        for _ in 0..3 {
+            for tone in theme.track().tones {
+                sink.append(tone);
+            }
+            sink.sleep_until_end();
+        }
+    }
 
     Ok(())
 }
