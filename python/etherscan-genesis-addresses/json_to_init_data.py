7e258f2677e7997a077499ddec51694da6103e2b
veeso <christian.visintin@veeso.dev>
2023-07-28 11:43:51 +0200

    git + ipynb = :(

---
+++ python/etherscan-genesis-addresses/json_to_init_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import json
+from sys import argv
+
+if __name__ == "__main__":
+    json_file = argv[1]
+    output_file = argv[2]
+
+    with open(json_file, "r") as f:
+        records = json.loads(f.read())
+        f.close()
+
+    genesis_accounts = records["genesisAccounts"]
+
+    with open(output_file, "w") as f:
+        for account in genesis_accounts:
+            address = account["address"]
+            tokens = account["tokens"]
+            f.write(f'record {{ 0= "{address}"; 1= opt "{hex(int(tokens))}"}};\n')
+        f.close()
